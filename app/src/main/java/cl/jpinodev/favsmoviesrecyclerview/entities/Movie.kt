37c3e67750a683bcbb30ae67fe36defe14840ba5
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-05-13 11:39:11 -0400

    Changes at list of movies and director's textview width

--- app/src/main/java/cl/jpinodev/favsmoviesrecyclerview/entities/Movie.kt
+++ app/src/main/java/cl/jpinodev/favsmoviesrecyclerview/entities/Movie.kt
@@ -16,8 +16,8 @@ data class Movie(
             Movie("4", "Full metal jacket", 1987, "Stanley Kubrick", 4.3, "War"),
             Movie("5", "The Matrix", 1999, "Lana Wachowski", 3.7, "Action"),
             Movie("6", "The Lord of the Rings: The Return of the King", 2003, "Peter Jackson", 4.9, "Adventure"),
-            Movie("7", "The Lion King", 1994, "Roger Allers", 3.5, "Animation"),
-            Movie("8", "The Incredibles", 2004, "Brad Bird", 3.0, "Animation"),
+            Movie("7", "Dune", 2021, "Denis Villeneuve", 4.1, "Science Fiction"),
+            Movie("8", "Megamind", 2010, "Tom McGrath", 3.8, "Animation"),
             Movie("9", "The Silence of the Lambs", 1991, "Jonathan Demme", 3.6, "Crime"),
             Movie("10", "The Sixth Sense", 1999, "M. Night Shyamalan", 3.1, "Drama")
         )
