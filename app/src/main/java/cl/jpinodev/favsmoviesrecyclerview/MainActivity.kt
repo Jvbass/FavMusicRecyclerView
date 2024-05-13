af698bf5ae44e6f7dc44f47abd83a06aa0502727
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-05-13 11:28:10 -0400

    Main activity with fun initAdapter calling the MovieAdapter

--- app/src/main/java/cl/jpinodev/favsmoviesrecyclerview/MainActivity.kt
+++ app/src/main/java/cl/jpinodev/favsmoviesrecyclerview/MainActivity.kt
@@ -1,14 +1,35 @@
 package cl.jpinodev.favsmoviesrecyclerview
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import cl.jpinodev.favsmoviesrecyclerview.adapter.MovieAdapter
+import cl.jpinodev.favsmoviesrecyclerview.databinding.ActivityMainBinding
+import cl.jpinodev.favsmoviesrecyclerview.entities.Movie
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.recyclerMovies.layoutManager = LinearLayoutManager(this)
+    initAdapter()
+    }
+
+    private fun initAdapter() {
+        val movieAdapter = MovieAdapter()
+        binding.recyclerMovies.adapter = movieAdapter
+        movieAdapter.movies = Movie.dataMovies
+
+         //sismoAdaptador.sismos = Sismo.dataEmpty
+        if (movieAdapter.movies.isEmpty())
+            binding.empty.visibility = View.VISIBLE
+        else
+            binding.empty.visibility = View.GONE
     }
 }
